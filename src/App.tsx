83b84fcc0d521a80efe51ea913b440b23141fee1
pjh5474 <warwarsn@gmail.com>
2023-08-18 15:00:02 +0900

    2023-08-18 tTypescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100vw;
+	height: 100vh;
+`;
+
+const rotateAnimation = keyframes`
+	0% {
+		transform: rotate(0deg);
+		border-radius: 0px;
+	}
+	50% {
+		border-radius: 100px;
+	}
+	100% {
+		transform: rotate(360deg);
+		border-radius: 0px;
+	}
+`;
+
+const Emoji = styled.span`
+	font-size: 20px;
+	color: ${(props) => props.theme.textColor};
+`;
+
+const Box = styled.div`
+	width: 200px;
+	height: 200px;
+	background-color: ${(props) => props.theme.backgroundColor};
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	animation: ${rotateAnimation} 1s linear infinite;
+	${Emoji} {
+		&:hover {
+			font-size: 40px;
+		}
+		&:active {
+			opacity: 0;
+		}
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<Box>
+				<Emoji as="p">☆o(≧▽≦)o☆</Emoji>
+			</Box>
+			<Emoji>╰(*°▽°*)╯╰(*°▽°*)╯</Emoji>
+		</Wrapper>
+	);
 }
 
 export default App;
