a0b786272024ddc153017705dae14b597c6001ae
pjh5474 <warwarsn@gmail.com>
2023-08-21 16:32:24 +0900

    2023-08-21 Coin Styles

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -9,10 +9,35 @@ const Container = styled.div`
 `;
 
 const Header = styled.header`
-	height: 10vh;
+	height: 15vh;
 	display: flex;
 	align-items: center;
 	justify-content: center;
+`;
+
+const Overview = styled.div`
+	display: flex;
+	justify-content: space-between;
+	background-color: rgba(0, 0, 0, 0.5);
+	padding: 10px 20px;
+	border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	span:first-child {
+		font-size: 10px;
+		font-weight: 400;
+		text-transform: uppercase;
+		margin-bottom: 5px;
+	}
+`;
+
+const Description = styled.p`
+	margin: 20px 0px;
 `;
 
 const Title = styled.h1`
@@ -111,9 +136,41 @@ function Coin() {
 	return (
 		<Container>
 			<Header>
-				<Title>{state?.name || "Loading..."} </Title>
+				<Title>
+					{state?.name ? state.name : loading ? "Loading..." : info?.name}{" "}
+				</Title>
 			</Header>
-			{loading ? <Loader>Loading...</Loader> : null}
+			{loading ? (
+				<Loader>Loading...</Loader>
+			) : (
+				<>
+					<Overview>
+						<OverviewItem>
+							<span>Rank :</span>
+							<span>{info?.rank}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Symbol :</span>
+							<span>{info?.symbol}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Open Source :</span>
+							<span>{info?.open_source ? "Yes" : "No"}</span>
+						</OverviewItem>
+					</Overview>
+					<Description>{info?.description}</Description>
+					<Overview>
+						<OverviewItem>
+							<span>Total Supply :</span>
+							<span>{priceInfo?.total_supply}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Max Supply :</span>
+							<span>{priceInfo?.max_supply}</span>
+						</OverviewItem>
+					</Overview>
+				</>
+			)}
 		</Container>
 	);
 }
